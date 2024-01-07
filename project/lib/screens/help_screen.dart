ffdc753b52cac3eb7968480c9ac1932d59576f73
Navindu Dissanayake <navindu158@gmail.com>
2024-01-08 01:02:24 +0530

    firebase sdk

--- project/lib/screens/help_screen.dart
+++ project/lib/screens/help_screen.dart
@@ -12,32 +12,6 @@ class HelpScreen extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-<<<<<<< Updated upstream
-          SizedBox(
-            height: 0,
-          ),
-          Column(
-            children: [
-              Text(
-                "Help Screen",
-                style: TextStyle(
-                  fontSize: 28,
-                  color: Theme.of(context).colorScheme.secondary,
-                ),
-              ),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
-                child: Text(
-                  """CareMe\n is a great tool to have in case of an emergency.
-When it comes to emergencies related to health, time is of the essence,
-this is an essential tool to have at your fingertips in case of any unexpected health emergencies.
-With a few clicks on your phone, you can quickly access the necessary resources to get your needed help.""",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 24,
-                    color: Theme.of(context).colorScheme.secondary,
-=======
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
@@ -104,13 +78,11 @@ With a few clicks on your phone, you can quickly access the necessary resources 
                       fontSize: 16,
                       color: Colors.white,
                     ),
->>>>>>> Stashed changes
                   ),
                 ),
               ),
             ],
           ),
-<<<<<<< Updated upstream
           Container(
             color: const Color.fromARGB(255, 107, 145, 196),
             padding: const EdgeInsets.symmetric(vertical: 10),
@@ -141,8 +113,6 @@ With a few clicks on your phone, you can quickly access the necessary resources 
               ],
             ),
           ),
-=======
->>>>>>> Stashed changes
         ],
       ),
     );
