c90d15130d3ed3127ee86a00a80f13032e573e2b
Navindu Dissanayake <navindu158@gmail.com>
2024-01-19 22:28:06 +0530

    Update home_screen.dart

--- project/lib/screens/home_screen.dart
+++ project/lib/screens/home_screen.dart
@@ -60,195 +60,197 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Theme.of(context).colorScheme.primaryContainer,
       ),
       drawer: MainDrawer(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          // Image.asset(
-          //   'assets/images/icon.png',
-          //   width: 80,
-          // ),
-          SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-            child: Container(
-              padding: EdgeInsets.all(8),
-              decoration: BoxDecoration(
-                color: Colors.transparent,
-                border: Border.all(color: Colors.black, width: 2),
-                borderRadius: BorderRadius.circular(50),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(
-                    Icons.medical_services_outlined,
-                    size: 40,
-                    color: Color.fromARGB(255, 255, 60, 60),
-                  ),
-                  SizedBox(width: 10),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Ambulance_Number',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Ambulance',
-                        style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Color.fromARGB(155, 136, 136, 136)),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  CircleAvatar(
-                    backgroundColor: Color.fromARGB(155, 136, 136, 136),
-                    radius: 25,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(height: 40),
-          Container(
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  offset: Offset(0, 8),
-                  color: Color.fromARGB(155, 64, 64, 64),
-                  spreadRadius: 5,
-                  blurRadius: 10,
-                ),
-              ],
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: Color.fromARGB(255, 255, 255, 255),
-                width: 5,
-              ),
-            ),
-            child: GestureDetector(
-              onTap: () {
-                _changeScreenSubmission();
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            // Image.asset(
+            //   'assets/images/icon.png',
+            //   width: 80,
+            // ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
               child: Container(
-                height: 275,
-                padding: EdgeInsets.symmetric(vertical: 30),
+                padding: EdgeInsets.all(8),
                 decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: Color.fromARGB(255, 255, 88, 88),
+                  color: Colors.transparent,
+                  border: Border.all(color: Colors.black, width: 2),
+                  borderRadius: BorderRadius.circular(50),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Icon(
-                      Icons.person_add_alt_1_sharp,
-                      color: Colors.white,
-                      size: 100,
+                      Icons.medical_services_outlined,
+                      size: 40,
+                      color: Color.fromARGB(255, 255, 60, 60),
                     ),
-                    Center(
-                      child: Text(
-                        "NEW PATIENT",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 35,
-                          fontWeight: FontWeight.bold,
+                    SizedBox(width: 10),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Ambulance_Number',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
+                        Text(
+                          'Ambulance',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Color.fromARGB(155, 136, 136, 136)),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    CircleAvatar(
+                      backgroundColor: Color.fromARGB(155, 136, 136, 136),
+                      radius: 25,
                     ),
                   ],
                 ),
               ),
             ),
-          ),
-          SizedBox(height: 60),
-          Container(
-            decoration: BoxDecoration(
-              // borderRadius: BorderRadius.circular(8),
-              color: Color.fromARGB(255, 107, 145, 196),
-            ),
-            padding: EdgeInsets.symmetric(vertical: 16),
-            width: double.infinity,
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Card(
-                    color: const Color.fromARGB(255, 20, 96, 165),
-                    child: ListTile(
-                      onTap: _changeScreenPatients,
-                      title: Center(
+            SizedBox(height: 20),
+            Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 8),
+                    color: Color.fromARGB(155, 64, 64, 64),
+                    spreadRadius: 5,
+                    blurRadius: 10,
+                  ),
+                ],
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  width: 5,
+                ),
+              ),
+              child: GestureDetector(
+                onTap: () {
+                  _changeScreenSubmission();
+                },
+                child: Container(
+                  height: 275,
+                  padding: EdgeInsets.symmetric(vertical: 30),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 255, 88, 88),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.person_add_alt_1_sharp,
+                        color: Colors.white,
+                        size: 100,
+                      ),
+                      Center(
                         child: Text(
-                          "Patient List",
+                          "NEW PATIENT",
+                          textAlign: TextAlign.center,
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: 25,
+                            fontSize: 35,
                             fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Container(
+              decoration: BoxDecoration(
+                // borderRadius: BorderRadius.circular(8),
+                color: Color.fromARGB(255, 107, 145, 196),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 16),
+              width: double.infinity,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Card(
+                      color: const Color.fromARGB(255, 20, 96, 165),
+                      child: ListTile(
+                        onTap: _changeScreenPatients,
+                        title: Center(
+                          child: Text(
+                            "Patient List",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 16),
-                  child: Card(
-                    color: const Color.fromARGB(255, 20, 96, 165),
-                    child: ListTile(
-                      onTap: _changeScreenHospitals,
-                      title: Center(
-                        child: Text(
-                          "Hospital List",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 25,
-                            fontWeight: FontWeight.bold,
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Card(
+                      color: const Color.fromARGB(255, 20, 96, 165),
+                      child: ListTile(
+                        onTap: _changeScreenHospitals,
+                        title: Center(
+                          child: Text(
+                            "Hospital List",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
                       ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Need some help ? ',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+                const SizedBox(width: 4),
+                GestureDetector(
+                  onTap: _changeScreenHelp,
+                  child: Text(
+                    'Help',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ],
             ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Need some help ? ',
-                style: TextStyle(color: Colors.grey[700]),
-              ),
-              const SizedBox(width: 4),
-              GestureDetector(
-                onTap: _changeScreenHelp,
-                child: Text(
-                  'Help',
-                  style: TextStyle(
-                    color: Theme.of(context).colorScheme.primary,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 10,
-          )
-        ],
+            SizedBox(
+              height: 10,
+            )
+          ],
+        ),
       ),
     );
   }
